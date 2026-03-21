c74d323d6a694a330ee691d633ee85d397fcfebc
Travis Dunbar <travis.j.dunbar@gmail.com>
2026-03-21 06:47:18 -0500

    adding path landscape or portrait

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
+	"math"
 	"mime"
 	"net/http"
 	"os"
+	"os/exec"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -82,6 +86,21 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 	}
 
 	key := getAssetPath(mediaType)
+	aspectRatio, err := getVideoAspectRatio(tempFile.Name())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not determine video aspect ratio", err)
+		return
+	}
+
+	switch aspectRatio {
+	case "16:9":
+		key = "landscape/" + key
+	case "9:16":
+		key = "portrait/" + key
+	default:
+		key = "other/" + key
+	}
+
 	_, err = cfg.s3Client.PutObject(r.Context(), &s3.PutObjectInput{
 		Bucket:      aws.String(cfg.s3Bucket),
 		Key:         aws.String(key),
@@ -102,4 +121,52 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 	}
 
 	respondWithJSON(w, http.StatusOK, video)
+}
+
+func getVideoAspectRatio(filePath string) (string, error) {
+	cmd := exec.Command("ffprobe", "-v", "error", "-print_format", "json", "-show_streams", filePath)
+	output, err := cmd.Output()
+	if err != nil {
+		return "", fmt.Errorf("ffprobe failed: %w", err)
+	}
+
+	type ffprobeStream struct {
+		CodecType string `json:"codec_type"`
+		Width     int    `json:"width"`
+		Height    int    `json:"height"`
+	}
+
+	type ffprobeResponse struct {
+		Streams []ffprobeStream `json:"streams"`
+	}
+
+	var data ffprobeResponse
+	if err := json.Unmarshal(output, &data); err != nil {
+		return "", fmt.Errorf("unable to parse ffprobe output: %w", err)
+	}
+
+	width, height := 0, 0
+	for _, stream := range data.Streams {
+		if stream.CodecType == "video" && stream.Width > 0 && stream.Height > 0 {
+			width = stream.Width
+			height = stream.Height
+			break
+		}
+	}
+
+	if width == 0 || height == 0 {
+		return "", fmt.Errorf("no valid video stream found in ffprobe output")
+	}
+
+	ratio := float64(width) / float64(height)
+	const tolerance = 0.08
+
+	if math.Abs(ratio-(16.0/9.0)) <= tolerance {
+		return "16:9", nil
+	}
+	if math.Abs(ratio-(9.0/16.0)) <= tolerance {
+		return "9:16", nil
+	}
+
+	return "other", nil
 }
